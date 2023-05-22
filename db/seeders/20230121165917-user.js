e7709b6f6cc74d89656f9b6c34c2429fe94a9388
yasinqurni <yalqurni@gmail.com>
2023-05-22 13:49:49 +0700

    fix seed

--- db/seeders/20230121165917-user.js
+++ db/seeders/20230121165917-user.js
@@ -5,11 +5,11 @@ module.exports = {
   async up (queryInterface, Sequelize) {
      await queryInterface.bulkInsert('users', [
       {
-        fullname: "user 1",
+        name: "user 1",
         code:  '$2y$08$vR4cMz4J5cREkhoHJc7Do.DXhuLHIReU/.xecoB0BjmokfVYcMNZO' //1212
       },
       {
-        fullname: "user 2",
+        name: "user 2",
         code:  '$2y$08$y1xJYwHgZH2NeMGE7MsPIuPsDuofGj.0VZLMf1qCumbou8IJFjQUK' //9532
       },
     ], {})
